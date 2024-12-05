8dcb0f613df88ab2ca01f7529fec82665f497719
vinter-raghav <raghav@vinter.co>
2024-12-06 04:45:07 +0530

    permission APIs are completed

--- role_and_permission/services.py
+++ role_and_permission/services.py
@@ -7,3 +7,23 @@ def create_permission_service(data):
     create_permission = utils.add_permission(data)
     data = PermissionSerializer(create_permission).data
     return data
+
+
+def view_permission_service(user, my_permissions):
+    if my_permissions == "true":
+        permissions = user.role.permissions.all()
+    else:
+        permissions = Permissions.objects.all()
+    data = PermissionSerializer(permissions, many=True).data
+    return data
+
+
+def delete_permission_service(data):
+    name = data["name"].strip()
+    final_name = name.upper().replace(" ", "_")
+    permission = Role.objects.filter(name=final_name).first()
+    if not permission:
+        raise Exception(f"No permission found with name - {name}")
+    else:
+        permission.delete()
+        return "DELETED"
--- role_and_permission/views.py
+++ role_and_permission/views.py
@@ -19,3 +19,29 @@ def create_permission(request):
         response = create_error_response(f"error --> {e}")
     return response
 
+
+@api_view(["GET"])
+@authorize([PermissionsEnum.VIEW_PERMISSION.name])
+def view_permission(request):
+    try:
+        user = request.auth_user
+        my_permissions = request.query_params.get("my_permissions")
+        permission = services.view_permission_service(user, my_permissions)
+        response = create_success_response("permission retrieved successfully !", permission)
+    except Exception as e:
+        response = create_error_response(f"error --> {e}")
+    return response
+
+
+@api_view(["POST"])
+@authorize([PermissionsEnum.DELETE_PERMISSION.name])
+def delete_permission(request):
+    try:
+        data = request.data
+        permission = services.delete_permission_service(data)
+        response = create_success_response("permission deleted successfully !", permission)
+    except Exception as e:
+        response = create_error_response(f"error --> {e}")
+    return response
+
+
